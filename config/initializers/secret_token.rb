d874c85b71d8d748c98dcd5bdbc0636f403e24b8
Emily Bache <emily.bache@gmail.com>
2011-04-19 18:26:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patients::Application.config.secret_token = '8adacdd265d7e84c962f603d33a3dbc110a9037f8d9ba83f09f8ba607d233706d8d1b844dbc5d453d7b791c5fdc43fb6b2956d8df9f3c9e8aa7deecd7868d074'
