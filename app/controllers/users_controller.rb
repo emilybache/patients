227c8eb0c9ca4defddf2b79fae642aa51aa6e7aa
Emily Bache <emily.bache@gmail.com>
2011-05-03 15:25:17 +0200

    users can sign up as admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @title = "Create Admin Account"
+    @user = User.new
   end
 
   def show
@@ -8,4 +9,14 @@ class UsersController < ApplicationController
     @title = @user.name
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Your application to become an Admin user was successful"
+      redirect_to @user
+    else
+      @title = "Admin Account"
+      render 'new'
+    end
+  end
 end
